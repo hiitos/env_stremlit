98862180fcd22fe83fa6cf15d74058a6dd95df5f
hitose kakazu <you@example.c>
2022-12-13 20:54:10 +0900

    画像分類のモデルが読み込めなかった場合に備えてアップローダーを追加した

--- src/pages/classifier.py
+++ src/pages/classifier.py
@@ -5,9 +5,9 @@ from func import func_classifier
 import torch
 import os
 
-# path = "./"
-# files = os.listdir(path)
-# print(files)        # ['dir1', 'dir2', 'file1', 'file2.txt', 'file3.jpg']
+path = "./"
+files = os.listdir(path)
+print(files)        # ['dir1', 'dir2', 'file1', 'file2.txt', 'file3.jpg']
 
 print("-------------classifier start -------------")
 
@@ -15,20 +15,30 @@ st.title("classifier")
 
 uploaded_file = st.file_uploader("画像をjpgかpngでアップロードしてください。")
 # ALLOWED_EXTENSIONS = {"png", "jpg", "jpeg", "gif"}
-submit_button = st.button('画像を分類する')
 
 if (uploaded_file is not None):
+    # モデルパス　app.pyと同じディレクトリに入れる
+    try:
+        model_path = "data/model_cnn1.pth"
+        torch.load(model_path, map_location=torch.device("cpu"))
+    except FileNotFoundError:
+        eroor_message = True
+        model_path = st.file_uploader("modelをアップロードしてください。")
+
+    if model_path is not None:
+        st.success("モデル読込成功")
+    else:
+        st.error("modelの読み込みに失敗したため、モデルをアップロードしてください")
+        
+    submit_button = st.button('画像を分類する')
+
     if submit_button:
         
         st.success("読み込み成功")
         st.image(uploaded_file)
-        
-        # モデルパス　app.pyと同じディレクトリに入れる
-        model_path = "data/model_cnn.pth"
         model = torch.load(model_path, map_location=torch.device("cpu"))
         st.write(func_classifier.result(uploaded_file,model))
     else:
         st.error("画像を分類するボタンを押してください")
 
-# "model_cnn.pth"
 print("-------------classifier end -------------")
